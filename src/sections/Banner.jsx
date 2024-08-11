fb2880feb899956fc8c942b1ab1bedc196026785
jonasgabrieel <jonasgabrieel@gmail.com>
2024-08-10 21:35:35 -0300

    desing banner

--- src/sections/Banner.jsx
+++ src/sections/Banner.jsx
@@ -5,15 +5,15 @@ export default function Banner (){
   return(
     <div className='w-full flex justify-center p-4 bg-white'>
       <div className=" w-full flex flex-col lg:flex-row justify-center items-center w-full max-w-[1256px] ">
-        <div className='flex flex-col items-center lg:items-start' >
-          <img src= { logo } alt='logo' title='logo-tokseg' className='w-64 mb-12'/>
+        <div className='flex flex-col items-center lg:items-start ' >
+          <img src= { logo } alt='logo' title='logo-tokseg' className='w-96 h-40 mb-8'/>
           <div className='flex flex-col items-center lg:items-start space-y-12 w-11/12	lg:w-full'>
             <h1 className='text-pretoTokSeg sm:text-7xl text-4xl text-center lg:text-start font-semibold '>TOKSEG, A MELHOR SOLUÇÃO PARA O SEU CONDOMÍNIO.</h1>
             <p className='text-pretoHover text-2xl	'>Simples, facial e completo é a nossa portaria inteligente.</p>
             <button className='hidden lg:flex btn btn-success rounded-full border-none text-white w-full max-w-[356px]'><a href="#form" className='scroll-smooth'>Orçamento sem compromisso</a></button>
           </div>
         </div>
-        <img src={banner} alt='image-banner' title='image-banner' className='w-full max-w-[484px]'/>
+        <img src={banner} alt='image-banner' title='image-banner' className='w-96 max-w-[484px]'/>
         <div className='w-full flex lg:hidden justify-center'>
           <button className='btn btn-success rounded-full border-none  text-white w-full max-w-[356px]'><a href="#form" className="scroll-smooth">Orçamento sem compromisso</a></button>
         </div>
