3f89ee77a2dc4165bf7094ad2f3719f7d3485ee6
jonasgabrieel <jonasgabrieel@gmail.com>
2025-02-01 23:43:14 -0300

    ajustes

--- src/componentes/Whatssap.jsx
+++ src/componentes/Whatssap.jsx
@@ -3,7 +3,7 @@ import whatsappLogo from '../assets/icones/whatsapp.svg'
 export default function Whatsapp(){
   return(
     <div className='fixed bottom-5 right-3'>
-      <a href="https://api.whatsapp.com/message/GCVHBXESHCOGN1?autoload=1&app_absent=0" target='_blank'>
+      <a href= "https://wa.me/557932512318" target='_blank'>
         <img src={whatsappLogo} alt="logo-whatsapp" className='w-16'/>
       </a>
     </div>
